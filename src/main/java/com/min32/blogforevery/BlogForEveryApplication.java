ae7fec73997ecebf57dda9d9923833be271f379c
강민승 <k4minseung@gmail.com>
2024-06-23 12:14:32 +0900

    kms -feat / first init

---
+++ src/main/java/com/min32/blogforevery/BlogForEveryApplication.java
@@ -0,0 +1,13 @@
+package com.min32.blogforevery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogForEveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogForEveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/min32/blogforevery/BlogForEveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min32.blogforevery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogForEveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
